3bc483bf73eb7bf11d06ac40361c5861bc5dddd0
Firmansyah Jaya Kusuma <88803824+Firmansjk@users.noreply.github.com>
2023-10-09 21:23:35 +0800

    node random

---
+++ Assets/Scripts/MapTraversingScripts/NodeControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+    [SerializeField] public GameObject[] nodes;
+    
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
