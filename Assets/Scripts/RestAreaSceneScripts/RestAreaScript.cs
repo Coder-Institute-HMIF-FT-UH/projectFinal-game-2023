7fe9d493df9db056541a1e307f697e6286708675
Firmansyah Jaya Kusuma <88803824+Firmansjk@users.noreply.github.com>
2023-10-14 09:17:43 +0800

    RestArea init

---
+++ Assets/Scripts/RestAreaSceneScripts/RestAreaScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestAreaScript : MonoBehaviour
+{
+    [SerializeField] public int pertambahanHP; 
+    void Start()
+    {
+        
+    }
+
+    public void Istirahat()
+    {
+        //Character Bertambah darahnya
+    }
+
+    public void LanjutPerjalanan()
+    {
+        //Langsung keluar tanpa ada pertambahan darah
+    }
+}
