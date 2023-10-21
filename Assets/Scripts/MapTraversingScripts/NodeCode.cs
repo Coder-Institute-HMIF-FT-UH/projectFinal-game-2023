bf8e2305c6a251dc7a54af1e3926e6088efb871f
hera <herawati21d@student.unhas.ac.id>
2023-10-21 15:51:14 +0800

    MapTraverColor

--- Assets/Scripts/MapTraversingScripts/NodeCode.cs
+++ Assets/Scripts/MapTraversingScripts/NodeCode.cs
@@ -10,15 +10,34 @@ public class NodeCode : MonoBehaviour
     [SerializeField] public string restAreaScene = "RestAreaScene";
     private bool hasLoadedScene = false;
 
+    // scene sebelumnya
+    private string previousScene;
+
+    // warna asli
+    private Color originalColor;
+
+
     void Start()
     {
         nodeTypeRandom = Random.Range(2, 4);
+
+        // Warna Node
+        if (PlayerPrefs.GetInt(name) == 1)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green; 
+        }
+
+        // Simpan warna asli
+        originalColor = GetComponent<SpriteRenderer>().color;
     }
 
     void OnMouseDown()
     {
         if (!hasLoadedScene)
         {
+            // Simpan adegan sebelumnya
+            previousScene = SceneManager.GetActiveScene().name;
+
             if (nodeTypeRandom == 2)
             {
                 SceneManager.LoadScene(battleScene, LoadSceneMode.Single);
@@ -29,7 +48,21 @@ public class NodeCode : MonoBehaviour
                 SceneManager.LoadScene(restAreaScene, LoadSceneMode.Single);
                 hasLoadedScene = true;
             }
+
+            // Node stay color
+            GetComponent<SpriteRenderer>().color = Color.black; 
+            PlayerPrefs.SetInt(name, 1);
+
+            // coba pindah otomatis ke adegan awal setelah 10 detik memilih node
+            Invoke("ReturnToPreviousScene", 10);
+
         }
     }
-}
 
+    // Fungsi agar kita dapat kembali ke scane sebelumnya
+    void ReturnToPreviousScene()
+    {
+        SceneManager.LoadScene(previousScene, LoadSceneMode.Single);
+
+    }
+}
--- Assets/Scripts/MapTraversingScripts/NodeControl.cs
+++ Assets/Scripts/MapTraversingScripts/NodeControl.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,48 @@ public class NodeControl : MonoBehaviour
     
     void Start()
     {
-        //DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
     {
         
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+    [SerializeField] public GameObject[] nodes;
+
+    // var u/mengecek apakah node dalam satu kolom sudah dipilih
+    public static bool isNodeSelectedInColumn = false;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (!isNodeSelectedInColumn)
+        {
+            // set variabel isNodeSelectedInColumn menjadi true setelah node dipilih
+            isNodeSelectedInColumn = true;
+        }
+    }
+    void ReturnToPreviousScene()
+    {
+        // set variabel isNodeSelectedInColumn menjadi false setelah kembali ke scane sebelumnya
+        isNodeSelectedInColumn = false;
     }
 }
